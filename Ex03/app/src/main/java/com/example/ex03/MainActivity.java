ef67be889d3ea0583c5c3a690086a1638553de5c
nguyenthanhcong03 <nguyenthanhcong03@hotmail.com>
2024-09-23 20:03:36 +0700

    Xu ly logic Ex03

--- Ex03/app/src/main/java/com/example/ex03/MainActivity.java
+++ Ex03/app/src/main/java/com/example/ex03/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.ex03;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +12,56 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText edta, edtb, edtc;
+    Button btncong, btntru, btnnhan, btnchia;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        edta = findViewById(R.id.edta);
+        edtb = findViewById(R.id.edtb);
+        edtc = findViewById(R.id.edtc);
+        btncong = findViewById(R.id.btncong);
+        btntru = findViewById(R.id.btntru);
+        btnnhan = findViewById(R.id.btnnhan);
+        btnchia = findViewById(R.id.btnchia);
+        btncong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int a = Integer.parseInt("0" + edta.getText());
+                int b = Integer.parseInt("0" + edtb.getText());
+                edtc.setText("a + b = " + (a+b));
+            }
+        });
+        btntru.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int a = Integer.parseInt("0" + edta.getText());
+                int b = Integer.parseInt("0" + edtb.getText());
+                edtc.setText("a - b = " + (a-b));
+            }
+        });
+        btnnhan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int a = Integer.parseInt("0" + edta.getText());
+                int b = Integer.parseInt("0" + edtb.getText());
+                edtc.setText("a x b = " + (a*b));
+            }
+        });
+        btnchia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int a = Integer.parseInt("0" + edta.getText());
+                int b = Integer.parseInt("0" + edtb.getText());
+                if (b == 0) {
+                    edtc.setText("b phai khac 0");
+                } else {
+                    edtc.setText("a / b = " + (a/b));
+                }
+
+            }
         });
     }
 }
