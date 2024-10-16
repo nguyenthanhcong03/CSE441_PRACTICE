1398c4093c06b89a2a38cd16d304978ce70fa588
nguyenthanhcong03 <nguyenthanhcong03@hotmail.com>
2024-10-16 12:51:12 +0700

    xu ly logic Ex07 Bai1

--- Ex07/Bai1/app/src/main/java/com/example/bai1/MainActivity.java
+++ Ex07/Bai1/app/src/main/java/com/example/bai1/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.bai1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +18,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        Button btn1 =(Button) findViewById(R.id.button1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(MainActivity.this,child_activity.class);
+                startActivity(intent1);
+            }
         });
     }
 }
--- Ex07/Bai1/app/src/main/java/com/example/bai1/child_activity.java
+++ Ex07/Bai1/app/src/main/java/com/example/bai1/child_activity.java
@@ -1,4 +1,26 @@
 package com.example.bai1;
 
-public class child_activity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class child_activity extends AppCompatActivity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.child_activity);
+        Button btn = (Button) findViewById(R.id.button1);
+        TextView txt1 = (TextView) findViewById(R.id.textView1);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(child_activity.this,MainActivity.class);
+                startActivity(intent1);
+            }
+        });
+    }
 }
+
+
