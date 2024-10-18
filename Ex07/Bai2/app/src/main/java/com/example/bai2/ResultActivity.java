9e1fcb9325d270d6a1a5e9a3b8bb51cc3aa6915f
nguyenthanhcong03 <nguyenthanhcong03@hotmail.com>
2024-10-18 20:48:25 +0700

    cap nhat logic Ex07 bai2

--- Ex07/Bai2/app/src/main/java/com/example/bai2/ResultActivity.java
+++ Ex07/Bai2/app/src/main/java/com/example/bai2/ResultActivity.java
@@ -2,6 +2,7 @@ package com.example.bai2;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -22,18 +23,30 @@ public class ResultActivity extends AppCompatActivity {
         setContentView(R.layout.activity_result);
         txtKQ = findViewById(R.id.txtKQ);
         btnBack = findViewById(R.id.btnBack);
-//nhận intent
+
+        //nhận intent
         Intent myIntent = getIntent();
-//lay bundle khoi intent
+        //lay bundle khoi intent
         Bundle myBundle = myIntent.getBundleExtra("mypackage");
         //lay du lieu khỏi bundle
         int a = myBundle.getInt("soa");
         int b = myBundle.getInt("sob");
 
-        Sting nghiem = "";
-        if(a == 0 & b==0){
+        String nghiem = "";
+        if(a == 0 && b==0){
             nghiem = "PT vo so nghiem";
+        } else if (a==0 && b != 0) {
+            nghiem = "PT vô nghiệm";
+        }else {
+            nghiem = "Ngiệm PT = "+ (-1.0)*b/a;
         }
+        txtKQ.setText(nghiem);
 
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
