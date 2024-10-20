3293f6e610e52a56114de1a254cdb811fc6a4872
nguyenthanhcong03 <nguyenthanhcong03@hotmail.com>
2024-10-20 21:22:45 +0700

    xu ly logic Ex13 Bai1

--- Ex13/Bai1/app/src/main/java/com/example/bai1/MainActivity.java
+++ Ex13/Bai1/app/src/main/java/com/example/bai1/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.bai1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +12,34 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class MainActivity extends AppCompatActivity {
+    //Khai báo ListView
+    String namephone[] ={"Điện thoại Iphone 12", "Điện thoại SamSung S20","Điện thoại Nokia 6","Điện thoại Bphone 2020","Điện thoại Oppo 5","Điện thoại VSmart joy2"};
+    int imagephone[] = {R.drawable.ip,R.drawable.ss,R.drawable.htc,R.drawable.lg, R.drawable.wp,R.drawable.sky};
+    ArrayList<Phone> mylist; // Khai báo mảng chính
+    MyArrayAdapter myadapter; //Khai báo Adapter
+    ListView lv; //Khai báo Listview
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        lv = findViewById(R.id.lv);
+        mylist = new ArrayList<>();
+        for (int i = 0; i <namephone.length;i++)
+        {
+            mylist.add(new Phone(namephone[i],imagephone[i]));
+        }
+        myadapter = new MyArrayAdapter(this,R.layout.layout_listview,mylist);
+        lv.setAdapter(myadapter);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent myintent = new Intent(MainActivity.this,SubActivity.class);
+                myintent.putExtra("name",namephone[position]);
+                startActivity(myintent);
+            }
         });
     }
 }
--- Ex13/Bai1/app/src/main/java/com/example/bai1/SubActivity.java
+++ Ex13/Bai1/app/src/main/java/com/example/bai1/SubActivity.java
@@ -1,6 +1,8 @@
 package com.example.bai1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +11,14 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class SubActivity extends AppCompatActivity {
-
+    TextView txt_subphone;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_sub);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        txt_subphone = findViewById(R.id.txt_subphone);
+        Intent myintent = getIntent();
+        String namephone = myintent.getStringExtra("name");
+        txt_subphone.setText(namephone);
     }
 }
