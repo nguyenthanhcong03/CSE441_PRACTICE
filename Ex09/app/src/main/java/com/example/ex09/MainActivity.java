bce9c37d223bdb8585bfac93fc9c83f8a494ac84
nguyenthanhcong03 <nguyenthanhcong03@hotmail.com>
2024-10-20 09:41:13 +0700

    thiet ke giao dien Ex09

---
+++ Ex09/app/src/main/java/com/example/ex09/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.ex09;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    ImageButton btnplay, btnstop;
+    Boolean flag = true;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
---
+++ Ex09/app/src/main/java/com/example/ex09/MyService.java
@@ -0,0 +1,10 @@
+package com.example.ex09;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+
+public class MyService extends Service {
+    
+}
