dfaf68198a1d81469a6e93fd6d38a815b42f95ba
nguyenthanhcong03 <nguyenthanhcong03@hotmail.com>
2024-10-20 09:43:27 +0700

    xu ly logic Main activity Ex09

--- Ex09/app/src/main/java/com/example/ex09/MainActivity.java
+++ Ex09/app/src/main/java/com/example/ex09/MainActivity.java
@@ -18,6 +18,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        btnplay = findViewById(R.id.btnplay);
+        btnstop = findViewById(R.id.btnstop);
+        // Xử lý click
+        btnplay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Khai báo Intent công khai để khởi động Service
+                Intent intent1 = new Intent(MainActivity.this, MyService.class);
+                startService(intent1);
+                if (flag == true)
+                {
+                    btnplay.setImageResource(R.drawable.baseline_stop_circle_24);
+                    flag = false;
+                }
+                else
+                {
+                    btnplay.setImageResource(R.drawable.baseline_play_circle_filled_24);
+                    flag = true;
+                }
+            }
+        });
+        btnstop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent2 = new
+                        Intent(MainActivity.this,MyService.class);
+                stopService(intent2);
+                btnplay.setImageResource(R.drawable.baseline_play_circle_filled_24);
+                flag = true;
+            }
+        });
     }
 }
