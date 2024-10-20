e41b3a99f72c009eff91a2f301909dd0804479e2
nguyenthanhcong03 <nguyenthanhcong03@hotmail.com>
2024-10-20 17:13:54 +0700

    xu ly logic Ex12_tieptheo Bai1

--- Ex12_tieptheo/Bai1/app/src/main/java/com/example/bai1/MainActivity.java
+++ Ex12_tieptheo/Bai1/app/src/main/java/com/example/bai1/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.bai1;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.MultiAutoCompleteTextView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +15,58 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    TextView selection;
+    AutoCompleteTextView singleComplete;
+    MultiAutoCompleteTextView multiComplete;
+    String arr []={"hà nội", "Huế", "Sài gòn",
+            "hà giang", "Hoi an", "Kien giang",
+            "Lâm dồng", "Long khánh"};
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        selection = (TextView) findViewById(R.id.selection);
+        //lấy đối tượng AutoCompleteTextView ra
+        singleComplete = (AutoCompleteTextView) findViewById(R.id.editauto);
+        ArrayAdapter myadapter = new ArrayAdapter<String>
+                (
+                        this,
+                        android.R.layout.simple_list_item_1,
+                        arr
+                );
+        //Thiết lập ArrayADapter
+        singleComplete.setAdapter(myadapter);
+
+        //Lây đối tuợng MultiAutoCompleteTextView ra
+        multiComplete = (MultiAutoCompleteTextView) findViewById(R.id.multiAutoCompleteTextView1);
+        //Thiết lập ArrayADapter
+        multiComplete.setAdapter(new ArrayAdapter<String>
+
+                (
+                        this,
+                        android.R.layout.simple_list_item_1,
+                        arr
+                ));
+
+        //Đối với MultiAutoCompleteTextView bắt buộc phải gọi dòng lệnh này
+        multiComplete.setTokenizer(new MultiAutoCompleteTextView.CommaTokenizer());
+
+        singleComplete.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
         });
     }
 }
