d4a0c8f4bfca6ba9ded44eef1fe85aa7e791b635
nguyenthanhcong03 <nguyenthanhcong03@hotmail.com>
2024-10-20 08:52:48 +0700

    xu ly logic Ex08 bai1

--- Ex08/Bai1/app/src/main/java/com/example/bai1/MainActivity.java
+++ Ex08/Bai1/app/src/main/java/com/example/bai1/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.bai1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +12,32 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnCallPhone, btnSendSMS;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        btnCallPhone = findViewById(R.id.btncallphone);
+        btnSendSMS = findViewById(R.id.btnsendsms);
+        btnCallPhone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Tạo mới một đối tượng intent
+                Intent intent1 =new Intent(MainActivity.this,CallPhoneActivity.class);
+                //Thực thi Intent1
+                startActivity(intent1);
+            }
+        });
+
+        btnSendSMS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Tạo mới một đối tượng intent
+                Intent intent2 =new Intent(MainActivity.this,SendSMSActivity.class);
+                //Thực thi Intent1
+                startActivity(intent2);
+            }
         });
     }
 }
--- Ex08/Bai1/app/src/main/java/com/example/bai1/SendSMSActivity.java
+++ Ex08/Bai1/app/src/main/java/com/example/bai1/SendSMSActivity.java
@@ -1,6 +1,12 @@
 package com.example.bai1;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +15,28 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class SendSMSActivity extends AppCompatActivity {
-
+    EditText edtsms;
+    Button btnback2;
+    ImageButton btnsendsms;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_send_smsactivity);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        edtsms = (EditText) findViewById(R.id.edtsms);
+        btnback2 = (Button) findViewById(R.id.btnback2);
+        btnsendsms = (ImageButton) findViewById(R.id.btnsms);
+        btnsendsms.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent callintent = new Intent(Intent.ACTION_SENDTO, Uri.parse("smsto:"+edtsms.getText().toString()));
+                startActivity(callintent);
+            }
+        });
+        btnback2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
         });
     }
 }
