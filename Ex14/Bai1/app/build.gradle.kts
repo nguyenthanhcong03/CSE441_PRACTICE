139e8f3e5066f0e3712a0262b2c77d2d0aee2da2
nguyenthanhcong03 <nguyenthanhcong03@hotmail.com>
2024-10-20 23:48:25 +0700

    cap nhat giao dien Ex14 bai1

--- Ex14/Bai1/app/build.gradle.kts
+++ Ex14/Bai1/app/build.gradle.kts
@@ -34,9 +34,6 @@ android {
 dependencies {
 
     implementation(libs.appcompat)
-    implementation(libs.material)
-    implementation(libs.activity)
-    implementation(libs.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
